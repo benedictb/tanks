017c9926277fdf6b9e8f483d0688601c99cf56aa
tfrances <tfrances@nd.edu>
2017-05-06 00:24:32 -0400

    added explosion and object removal from gamespace

--- mid_bullet.py
+++ mid_bullet.py
@@ -12,30 +12,41 @@ class MidBullet(pygame.sprite.Sprite):
         self.image = pygame.image.load('mid_bullet.png')
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
-
+        # get angle of trajectory
         self.x, self.y = pygame.mouse.get_pos()
         self.angle = 360 - math.atan2(self.y - self.rect.centery, self.x - self.rect.centerx) * 180 / math.pi
-
+        # use angle to get x and y velocity
         self.velx = speed * math.cos(math.radians(360 - self.angle))
         self.vely = speed * math.sin(math.radians(360 - self.angle))
         self.vel = np.asarray([self.velx, self.vely])
         print("velx: " + str(self.velx) + " vely: " + str(self.vely))
-        #self.vel = np.asarray([np.cos(self.angle)*speed, np.sin(self.angle)*speed], dtype='float32')
-        #self.vel = np.asarray([20, -20]) #testing
+        # self.vel = np.asarray([np.cos(self.angle)*speed, np.sin(self.angle)*speed], dtype='float32')
         self.wind = wind
         self.gs = gs
+        self.explosiongif = 0
 
     def tick(self):
+        # get height of ground at current x value
         ground = 595 - int(self.gs.gameobjects[0].heights[(self.rect.centerx / PIXEL_SIZE + 5) % 199] * 5)
+        # if not hit anything, keep going
         if self.pos[1] <= ground:
             acc = self.wind + GRAVITY
             self.vel[0] += acc[0]
             self.vel[1] += acc[1]
             self.pos[0] += self.vel[0]
             self.pos[1] += self.vel[1]
-            #self.rect.move(self.velx, self.vely)
             self.pos[0] = self.pos[0] % 1000
             self.rect.center = self.pos
+        # explode on contact
+        elif self.explosiongif < 17:
+            if self.explosiongif < 10:
+                self.image = pygame.image.load('explosion/frames00' + str(self.explosiongif) + 'a.png')
+            else:
+                self.image = pygame.image.load('explosion/frames0' + str(self.explosiongif) + 'a.png')
+            self.explosiongif += 1
+        # remove from game when explosion is over
+        else:
+            self.gs.gameobjects.remove(self)
 
     def update(self):
         self.gs.screen.blit(self.image, self.rect)
