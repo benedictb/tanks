785ba6623ee2b6e03f9b9a66a16c91ddba056123
tfrances <tfrances@nd.edu>
2017-05-05 22:02:53 -0400

    tank follows ground level

--- tank.py
+++ tank.py
@@ -1,16 +1,32 @@
 import pygame
 import numpy as np
-
+GRAVITY = np.asarray([0, 2])
 
 class MidTank(pygame.sprite.Sprite):
     def __init__(self, gs, pos):
         super().__init__()
         self.pos = np.asarray(pos)
-        self.image = './media/mid_tank.png'
+        self.image = pygame.image.load('mid_tank.png')
+        self.rect = self.image.get_rect()
+        self.rect.centerx = pos[0]
         self.gs = gs
+        self.vel = np.asarray([0, 1])
+        self.acc = GRAVITY
 
     def tick(self):
-        self.rect.center = self.pos
+        #if self.rect.centery <= 300:
+        #    self.vel += self.acc
+        #    self.pos += self.vel
+        #    self.rect.center = self.pos
+        #else:
+        #    self.vel = ([0, 0])
+        self.pos[0] = self.pos[0] % 1000
+        self.rect.centery = 550 - self.gs.gameobjects[0].heights[(self.rect.centerx / 5 + 5) % 200] * 5
+        print("height:" + str(self.gs.gameobjects[0].heights[self.rect.centerx / 5]))
+
+    def round(self, y):
+        return int( 5 * round(float(y)/5))
+
 
     def update(self):
         self.gs.screen.blit(self.image, self.rect.center)
