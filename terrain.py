f775794040e460fefed8e8db3297c81b9b5aafc1
benedictb <becker.benedictj@gmail.com>
2017-05-05 21:30:40 -0400

    more terrain

--- terrain.py
+++ terrain.py
@@ -3,8 +3,8 @@
 import pygame
 import numpy as np
 import random
-SQUARE_SIDE = 5
-ROCK_LEVEL = 20
+PIXEL_SIZE = 5
+ROCK_LEVEL = 3
 
 # Not sure if this is a sprite..
 class Terrain(pygame.sprite.Sprite):
@@ -13,49 +13,62 @@ class Terrain(pygame.sprite.Sprite):
         self.gs = gs
         self.screenHeight = gs.height
         self.screenWidth = gs.width
-        self.heights = [10] * int(self.screenWidth/SQUARE_SIDE)
-        self.gen_terrain()
+        self.heights = [10] * int(self.screenWidth / PIXEL_SIZE)
         self.dirt = (117, 76, 16)
         self.rock = (141, 155, 141)
         self.grass = (48, 219, 48)
+        self.gen_terrain()
+
         # print(self.heights)
 
     def tick(self):
         pass
 
     def update(self):
-        for i, h in enumerate(self.heights):
-            i*= SQUARE_SIDE
+        for i in range(0, len(self.gmap)):
+            for j in range (0, len(self.gmap[i])):
+                cell = self.gmap[i,j]
+                if cell[0]:
+                    self.gs.screen.fill(cell[1:], ((i * PIXEL_SIZE, self.screenHeight - j * PIXEL_SIZE), (PIXEL_SIZE, PIXEL_SIZE)))
 
-            for k in range(ROCK_LEVEL,h):
-                self.gs.screen.fill(self.dirt, ((i,self.screenHeight-k), (SQUARE_SIDE, SQUARE_SIDE)))
 
-            for j in range(0,ROCK_LEVEL+random.randint(-5,5)):
-                self.gs.screen.fill(self.rock, ((i,self.screenHeight-j), (SQUARE_SIDE, SQUARE_SIDE)))
 
-            for l in range(h-ROCK_LEVEL, h):
-                self.gs.screen.fill(self.grass, ((i, self.screenHeight - l), (SQUARE_SIDE, SQUARE_SIDE)))
 
     def gen_terrain(self):
         self.heights[0] = 100
         self.heights[-1] = 100
-        length = int(self.screenWidth / SQUARE_SIDE)
+        length = int(self.screenWidth / PIXEL_SIZE)
+        height = int(self.screenHeight / PIXEL_SIZE)
         mid = int(length/2)
         for i in range(1,mid):
             prev = self.heights[i-1]
-            addition = random.uniform(-8,10)
+            addition = random.uniform(-8,13)
             self.heights[i] = int(addition + prev)
 
         for j in range(length-2, mid, -1):
             prev = self.heights[j+1]
-            addition = random.uniform(-8,10)
+            addition = random.uniform(-8,13)
             self.heights[j] = int(addition+prev)
 
         self.heights[mid] = int((self.heights[mid+1] + self.heights[mid-1]) / 2)
-        self.heights *= SQUARE_SIDE
+        self.heights = np.floor_divide(np.asarray(self.heights, dtype=int), PIXEL_SIZE)
 
+        gmap = np.zeros((length, height, 4))
 
+        for i, h in enumerate(self.heights):
 
+            for j in range(ROCK_LEVEL,h):
+                gmap[i,j,0] = 1
+                gmap[i,j,1:] = self.dirt
 
+            for j in range(0, ROCK_LEVEL + random.randint(0, 2)):
+                gmap[i,j,0] = 1
+                gmap[i,j,1:] = self.rock
+
+            for j in range(h - ROCK_LEVEL, h):
+                gmap[i,j,0] = 1
+                gmap[i,j,1:] = self.grass
+
+        self.gmap = gmap
 
 
